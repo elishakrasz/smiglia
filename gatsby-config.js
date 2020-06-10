4306bf99b6f7774aca71eaa11e40d804a7cb2e27
Elisha Mallard <elishashestov@gmail.com>
2020-06-10 14:15:57 +0300

    contentful variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
@@ -13,6 +16,13 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images/`,
       },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
     },
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
